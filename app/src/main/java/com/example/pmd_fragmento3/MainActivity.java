10a2d76b2865ae75b5aadc737631451cba083f0d
sgalvez88 <sergio.galvez.reguera@iestubalcain.net>
2023-01-23 09:14:43 +0100

    ejercicio ok

--- app/src/main/java/com/example/pmd_fragmento3/MainActivity.java
+++ app/src/main/java/com/example/pmd_fragmento3/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.pmd_fragmento3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botonFragment = (Button) findViewById(R.id.button1);
+        Button botonFragment2 = (Button) findViewById(R.id.button2);
+
+        botonFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragmento fragmento = new Fragmento();
+                fragmentTransaction.add(R.id.contenedor, fragmento);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+        botonFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragmento2 fragmento2 = new Fragmento2();
+                fragmentTransaction.add(R.id.contenedor,fragmento2);
+                fragmentTransaction.commit();
+
+            }
+        });
+
+
     }
 }
